61e6b2b9359e53c66b2a9c78462502070387948b
Jais000 <98848405+Jais000@users.noreply.github.com>
2023-08-18 00:24:30 -0700

    Delete Dictionary directory

--- Dictionary/models/active.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const Commune = require('C:/Users/Jais/OneDrive/Projects/Dictionary/models/commune.js')
-const userSchema = new mongoose.Schema({
-    user: {type:mongoose.SchemaTypes.ObjectId, ref:'User'}
-})
-module.exports = mongoose.model('Active', userSchema)
