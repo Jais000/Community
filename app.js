6d6a09add128443974583001a7b4dcd6209661ed
jais <9000jais@gmail.com>
2023-08-19 02:15:10 -0700

    update

--- app.js
+++ app.js
@@ -2,42 +2,21 @@ import express from 'express'
 const app = express()
 import * as cql from './conn.js'
 import bodyParser from'body-parser'
-var activeId;
 var name = ''; 
 app.listen(3000,()=>{
-    console.log('Server running on 3000')
 })
 app.set('views','C://Users//Jais//OneDrive//Projects//Communities//views')
 app.set('view engine','ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-var activeUser; 
 app.get('',async (req,res)=>{
     const users = await cql.getUsers()
     res.render('home',{name:name})
 })
 
 
-app.get('/signin',async (req,res)=>{  
-    res.render('login')
-})
-app.post('/s',async (req,res)=>{
-    console.log(req.body.email)
-    var id = await cql.getUserIdByEmail(req.body.email)
-    activeId = id.id
-    var user = await cql.getUserById(activeId)
-    name = user.name 
-    console.log(name)
-    res.render('home',{name:name})
-})
-app.post('/submit',async (req,res)=>{
-    console.log(req.body)
-    var name = req.body.name
-    var email = req.body.email
-    var password = req.body.password
-    cql.createUser(name,password,email)
-    res.render('home')
-})
+
+
 
 /////////////////////////COMMUNITY/////////////////////////////////
 app.post('/search',async(req,res)=>{
@@ -50,20 +29,104 @@ app.post('/search',async(req,res)=>{
         if(comms[i].name.toLowerCase().includes(search) == true)
             {searched.push(comms[i])}
     }
-    console.log(searched)
     searched.forEach(comm=>{
         names.push(comm.name)
         ids.push(comm.id)
     })
-    console.log(names,ids)
     res.render('searchcommunities',{names:names,ids:ids})
 
 })
 
 app.post('/submitcomm', async(req,res)=>{  
     cql.createCommunity(req.body.name)
-    res.render('home', {name:name})
+    var logged = await cql.getActiveId()
+    res.render('home', {name:name, status:logged[0].isloggedin,id:logged[0].id})
+})
+
+//route to community page//
+app.get('/community/:id', async(req,res)=>{
+    var [comm] = await cql.getCommunityById(req.params.id.split(':')[1])
+    console.log(comm)
+    res.render('communityhome',{name:comm.name,id:comm.id})
+})
+//route to my communities page//
+app.get('/MyCommunities/:id', async(req,res)=>{
+    var comms = await cql.getCommunitiesByUserId(req.params.id.split(':')[1])
+    console.log(comms)
+    res.render('mycommunities',{comms:comms})
+})
+
+//create an Event//
+app.post('/create_event/:id',async(req,res)=>{
+    var name = req.body.name
+    var time = req.body.time
+    var loc = req.body.loc
+    var string = '{"name":"'+name+'","time":"'+time+'" ,"loc":"'+loc+'"}' 
+    cql.createEvent(req.params.id.split(':')[1], string)
+
+})
+
+//route to createCommunity//
+app.get('/create_community', async(req,res)=>{
+    res.render('create_community');
+})
+
+///////////////////////////USER////////////////////////////////////
+
+//Sign up//
+app.post('/submit',async (req,res)=>{
+    var name = req.body.name
+    var email = req.body.email
+    var password = req.body.password
+    cql.createUser(name,password,email)
+    res.render('home')
 })
 
 
-///////////////////////////USER////////////////////////////////////
+//route to login//
+app.get('/signin',async (req,res)=>{  
+    res.render('login')
+})
+
+//login//
+app.post('/s',async (req,res)=>{
+    var id = await cql.getUserIdByEmail(req.body.email)
+    console.log(id.id)
+    var user = await cql.getUserById(id)
+    cql.signin(id.id)
+    name = user.name 
+    var logged = await cql.getActiveId()
+    console.log(logged)
+    res.render('home',{name:name,status:logged[0].isloggedin,id:logged[0].id})
+})
+
+//logout//
+app.post('/signout',async (req,res)=>{
+    await cql.signout()
+    var [rows]=await cql.getActiveId()
+    res.render('home',{status:rows.isloggedin,id:rows.id})
+})
+
+//join community//
+app.post('/join/:id',async(req,res)=>{
+    var comm_id = req.params.id.split(':')[1]
+    var [comm] = await cql.getCommunityById(req.params.id.split(':')[1])
+    var activeId = await cql.getActiveId()
+    cql.joinCommunity(activeId[0].id,comm_id)
+
+    res.render('communityhome',{name:comm.name,id:comm.id})
+})
+
+//Render events for the user//
+app.get('/user_events/:id',async(req,res)=>{
+    var j_events= []
+    var comms = []
+    var events = await cql.getCommunityEventsByUserId(req.params.id.split(':')[1])
+    for(var i = 0; i<events.length;i++){
+        j_events.push(JSON.parse(events[i].event))
+        console.log(events[i])
+        comms.push({name: events[i].name, id: events[i].id})
+        console.log(comms)
+    }
+    res.render('user_events', {events:j_events,comms:comms})
+})
--- conn.js
+++ conn.js
@@ -9,17 +9,87 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise()
 
-export async function getUsers(){
-    const [rows] = await pool.query('SELECT * FROM users')
+/////////////////////////COMMUNITY/////////////////////////////////
+
+export async function createCommunity(name){
+    await pool.query(`
+        INSERT INTO communes (name)
+        VALUES (?)
+    `,[name])
+}
+export async function getCommunities(){
+    const [rows] = await pool.query(`
+        SELECT * FROM communes
+    `)
     return rows
 }
-export async function getUser(id){
+export async function getCommunityById(id){
     const [rows] = await pool.query(`
-        SELECT * 
-        FROM users 
+        SELECT * FROM communes
         WHERE id = ?
-    `, [id])
-    return rows[0]
+    `,[id])
+    return rows
+}
+
+export async function getCommunitiesByUserId(user_id){
+    const [rows] = await pool.query(`
+        SELECT commune_id, communes.name from users_has_communes 
+        JOIN users ON users.id = users_has_communes.user_id 
+        JOIN communes ON communes.id = users_has_communes.commune_id 
+        WHERE users.id = ?
+    `,[user_id])
+    return rows
+}
+export async function createEvent(comm_id, event){
+    await pool.query(`
+        INSERT INTO events (event)
+        VALUES (?) 
+    `,[event])
+    var [event_id] = await pool.query(`
+        SELECT * FROM events
+    `)
+    var cnt = 0;
+    for(var i = 0;i<event_id.length;i++){
+        if(event_id[i].id > cnt){cnt = event_id[i].id  }
+    }
+    await pool.query(`
+        insert into events_has_communes (events_id,communes_id) 
+        values (?,?)
+    `,[cnt, comm_id])
+}
+
+///////////////////////////USER////////////////////////////////////
+export async function signin(id){
+    await pool.query(`
+        UPDATE activeid
+        SET id = ?, isloggedin = ?
+    `,[id,1])
+}
+export async function signout(){
+    await pool.query(`
+        UPDATE activeid
+        SET id = ?, isloggedin = ?
+    `,[0,0])
+}
+
+export async function getActiveId(){
+    var [rows] = await pool.query(`
+        SELECT * FROM activeid
+    `)
+    return rows;
+}
+export async function createUser(name, password, email){
+    var [user] = await pool.query(`
+    INSERT INTO users (email, name, password)
+    VALUES (?,?,?)
+    `,[email, name, password])
+}
+
+export async function joinCommunity(user_id,comm_id){
+    await pool.query(`
+        INSERT INTO users_has_communes (user_id, commune_id)
+        VALUES (?,?)
+    `,[user_id,comm_id])
 }
 
 export async function getUserIdByEmail(email){
@@ -38,27 +108,23 @@ export async function getUserById(id){
     `, [id])
     return rows[0]
 }
-
-export async function createUser(name, password, email){
-    await pool.query(`
-        INSERT INTO users (email, name, password)
-        VALUES (?,?,?)
-    `,[email,name, password])
-}
-console.log(await getUsers())
-/////////////////////////COMMUNITY/////////////////////////////////
-
-export async function createCommunity(name){
-    await pool.query(`
-        INSERT INTO communes (name)
-        VALUES (?)
-    `,[name])
-}
-export async function getCommunities(){
-    const [rows] = await pool.query(`
-        SELECT * FROM communes
-    `)
+export async function getUsers(){
+    const [rows] = await pool.query('SELECT * FROM users')
     return rows
 }
-
-///////////////////////////USER////////////////////////////////////
+export async function getUser(id){
+    const [rows] = await pool.query(`
+        SELECT * 
+        FROM users 
+        WHERE id = ?
+    `, [id])
+    return rows[0]
+}
+export async function getCommunityEventsByUserId(user_id){
+    const [rows] = await pool.query(`
+    select communes.id,communes.name,events.id,events.event from events join events_has_communes on events_has_communes.events_id = events.id join communes on events_has_communes.communes_id = communes.id 
+    join users_has_communes on users_has_communes.commune_id = communes.id
+    join users on users.id = users_has_communes.user_id where users.id = ?;
+    `,[user_id])
+    return rows
+}
